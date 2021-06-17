04e03b776a8de2ddedeb54af5ce4c6a63169ab19
fabo893 <jose.rosa893@gmail.com>
2021-06-16 20:52:56 -0400

    comment for javascript code

--- image_preview.js
+++ image_preview.js
@@ -1,7 +1,9 @@
-const files = document.getElementById("image_uploads");
-const previewContainer = document.getElementById("imagePreview");
-const previewImage = previewContainer.querySelector(".preview_image");
-const previewText = previewContainer.querySelector(".preview_text");
+
+// First picture upload
+const files = document.getElementById("img_one");
+const previewContainer = document.getElementById("img_one_preview");
+const previewImage = previewContainer.querySelector(".preview_image_one");
+const previewText = previewContainer.querySelector(".preview_text_one");
 
 files.addEventListener("change", function () {
   const file = this.files[0];
@@ -18,5 +20,30 @@ files.addEventListener("change", function () {
     previewText.style.display = null;
     previewImage.style.display = null;
     previewImage.setAttribute("src", "");
+  }
+});
+
+
+// Second picture upload
+const files2 = document.getElementById("img_two");
+const previewContainer2 = document.getElementById("img_two_preview");
+const previewImage2 = previewContainer2.querySelector(".preview_image_two");
+const previewText2 = previewContainer2.querySelector(".preview_text_two");
+
+files2.addEventListener("change", function () {
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    previewText2.style.display = "none";
+    previewImage2.style.display = "block";
+    reader.addEventListener("load", function () {
+      console.log(this);
+      previewImage2.setAttribute("src", this.result);
+    });
+    reader.readAsDataURL(file);
+  } else {
+    previewText2.style.display = null;
+    previewImage2.style.display = null;
+    previewImage2.setAttribute("src", "");
   }
 });
